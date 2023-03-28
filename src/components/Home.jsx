f1d2fc98ab0f4937e38cb48d779bf75b4bac797d
charlesik <charlesikilo@gmail.com>
2023-03-28 14:44:32 +0100

    added playlist

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,58 +1,68 @@
-import React from 'react'
-import SongList from './SongList'
-import TopBar from './TopBar'
+import React from "react";
+import SongList from "./SongList";
+import TopBar from "./TopBar";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [songs, setSongs] = useState([]);
+  const [popSongs, setPopSongs] = useState([]);
+  const [playlist, setPlaylist] = useState([]);
+
+
+  useEffect(() => {
+    fetch("https://musica-api.up.railway.app/new")
+      .then((res) => res.json())
+      .then((data) => setSongs(data));
+  }, []);
+  useEffect(() => {
+    fetch("https://musica-api.up.railway.app/popular")
+      .then((res) => res.json())
+      .then((data) => setPopSongs(data));
+  }, []);
+  useEffect(() => {
+    fetch("https://musica-api.up.railway.app/playlist")
+      .then((res) => res.json())
+      .then((data) => setPlaylist(data));
+  }, []);
+  console.log(playlist)
+
+  const playlists = playlist.slice(3).map((playlist,index) => {
+    return (
+      <div className="bg-[#1A1E1F] flex h-[96px] w-[417px] p-2 justify-around rounded-2xl mb-3" key={index}>
+        <img src={playlist.cover} alt="" />
+        <div>
+          <p className="text-xl">{playlist.title}</p>
+        </div>
+      </div>
+    );
+  });
+
   return (
-    
-    <div className='w-full text-white ml-24 max-w-[92%]'>
-        <TopBar/>
-        <div className='flex justify-around  w-full'>
-        <div className='bg-[#609EAF] h-[373px] w-[686px] rounded-3xl flex text-white'>
-            <div className='w-1/2 pl-10 flex flex-col justify-around'>
-                <p>Currated Playlist</p>
-                <div>
-                <p className='text-3xl font-bold'>Hits</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
-                </div>
-                <p>33k Likes</p>
+    <div className="w-full text-white ml-24 max-w-[92%]">
+      <TopBar />
+      <div className="flex justify-around  w-full">
+        <div className="bg-[#609EAF] h-[373px] w-[686px] rounded-3xl flex text-white">
+          <div className="w-1/2 pl-10 flex flex-col justify-around">
+            <p>Currated Playlist</p>
+            <div>
+              <p className="text-3xl font-bold">Hits</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
             </div>
-            <div className='pl-10'>
-                <img src="kdot.png" alt="" className='max-h-full'/>
-            </div>
+            <p>33k Likes</p>
+          </div>
+          <div className="pl-10">
+            <img src="kdot.png" alt="" className="max-h-full" />
+          </div>
         </div>
 
-        <div className='text-white'>
-            <p className='text-2xl'>Top Charts</p><br />
-            <div className='bg-[#1A1E1F] flex h-[96px] w-[417px] p-2 justify-around rounded-2xl mb-3'>
-                <img src="head.png" alt="" />
-                <div>
-                <p className='text-xl'>Alte Cruise</p>
-                <p className='text-gray-300' >Spotify</p>
-                <p>2:10:15</p>
-                </div>
-            </div>
-            <div className='bg-[#1A1E1F] flex h-[96px] w-[417px] p-2 justify-around rounded-2xl mb-3'>
-                <img src="head.png" alt="" />
-                <div>
-                <p className='text-xl'>Alte Cruise</p>
-                <p className='text-gray-300' >Spotify</p>
-                <p>2:10:15</p>
-                </div>
-            </div>
-            <div className='bg-[#1A1E1F] flex h-[96px] w-[417px] p-2 justify-around rounded-2xl mb-3'>
-                <img src="head.png" alt="" />
-                <div>
-                <p className='text-xl'>Alte Cruise</p>
-                <p className='text-gray-300' >Spotify</p>
-                <p>2:10:15</p>
-                </div>
-            </div>
+        <div className="text-white">
+          <p className="text-2xl">Top Playlists</p>
+          <br />
+          {playlists}
         </div>
-        </div>
-        <SongList/>
-        <SongList/>
-
+      </div>
+      <SongList songs={songs} list="New Releases" />
+      <SongList songs={popSongs} list="Popular Releases" />
     </div>
-  )
+  );
 }
--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -1,28 +1,20 @@
-import React, { useState, useEffect } from "react";
 
-export default function SongList() {
-  const [songs, setSongs] = useState([]);
-
-  useEffect(() => {
-    fetch("https://musica-api.up.railway.app/popular")
-      .then((res) => res.json())
-      .then((data) => setSongs(data));
-  }, []);
-  console.log(songs);
-  const topCharts = songs.map((song, index) => {
+export default function SongList(props) {
+  
+  const topCharts = props.songs.map((song, index) => {
     return (
-      <div className="h-[153px] w-[153px] min-w-[153px] m-5">
+      <div className="h-[153px] w-[153px] min-w-[153px] m-5" key={index}>
         <img src={song.cover} alt="" className="w-[153px] h-[153px] rounded-3xl" />
         <p>{song.title}</p>
       </div>
     );
   });
+  
   return (
     <div className="">
-      <p>New Releases</p>
+      <p>{props.list}</p>
       <div className="bar flex justify-around overflow-x-auto h-[220px]">
         {topCharts}
-        
       </div>
     </div>
   );
